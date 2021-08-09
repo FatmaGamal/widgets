e19da9da877218d53382417b74b5b2a8d9f1a49b
FatmaGamal <hermonie.23@gmail.com>
2021-08-09 16:01:25 +0200

    WD / opening link in new tab via keyboard

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,11 @@ import React from 'react';
 const Link = ({className, href, children}) => {
 
     const onAnchorClick = (e) => {
+        // mac || windows
+        // boolean according to whether a key was held down while clicking by mouse
+        if (e.metaKey || e.ctrlKey) {
+            return ;
+        }
         e.preventDefault();
         window.history.pushState({}, '', href);
 
